5c2b6d8132ef6a605e0d8a3bcedf847f4036bd9b
Rosalie Prom <rosalinda.prom@gmail.com>
2020-04-06 21:20:35 +1000

    Added basic buttons and fixed fonts

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,28 @@
 import styled from "styled-components";
 
 const Layout = styled.div`
-  padding: 2rem;
+  margin: 0 auto;
+  background: ${props => props.theme.bg.secondary};
+  color: ${props => props.theme.text.primary};
 `;
+
+Layout.defaultProps = {
+  theme: {
+    bg: {
+      primary: "#eff0f5",
+      secondary: "#ffffff",
+      inset: "#e2e4e8",
+      input: "rgba(65,67,78,0.12)"
+    },
+    text: {
+      primary: "#050505",
+      secondary: "#2f3037",
+      tertiary: "#525560",
+      quarternary: "#9194a1",
+      placeholder: "rgba(82,85,96,0.5)",
+      onPrimary: "#ffffff"
+    }
+  }
+};
 
 export default Layout;
