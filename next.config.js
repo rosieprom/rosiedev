192e0d5d91ab7fb984a311df188ff066fd7cd679
Rosalie Prom <rosalinda.prom@gmail.com>
2020-04-16 21:17:34 +1000

    test

--- next.config.js
+++ next.config.js
@@ -1,10 +1,17 @@
 const withFonts = require("next-fonts");
-const path = require("path");
+// const path = require("path");
+
+// module.exports = {
+//   target: "serverless",
+//   webpack(config) {
+//     config.resolve.modules.push(path.resolve("./"));
+//     return config;
+//   },
+// };
 
 module.exports = withFonts({
   target: "serverless",
   webpack(config) {
-    config.resolve.modules.push(path.resolve("./"));
     return config;
   },
 });
